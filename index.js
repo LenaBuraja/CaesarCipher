8f915b4c9e286c05aae98d0639be0c237adc43cf
Lena <lenaLatkovich@gmail.com>
2018-08-24 00:50:19 +0300

    add exceptions

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import CaesarCipher from './lib/caesar-cipher';
 
 const cipher = new CaesarCipher(5);
-const a1 = cipher.encrypt('meSSage');
+const a1 = cipher.encrypt('m1eSSa ge');
 console.log(a1);
 console.log(cipher.decrypt(a1));
-cipher.shift = 6;
+cipher.shift = null;
 const a2 = cipher.encrypt('сОобщЕние');
 console.log(a2);
 console.log(cipher.decrypt(a2));
--- test/test.js
+++ test/test.js
@@ -1,34 +1,44 @@
 import CaesarCipher from '../lib/caesar-cipher';
+import assert from 'assert';
 
-describe('CaesarCipher', function() {
-	const text1 = 'meSSage';
-	const result1 = 'rjXXflj';
-	const text2 = 'сОобщЕние';
-	const result2 = 'чФфзяЛуол';
-	const cipher = new CaesarCipher(5);
-	const a1 = cipher.encrypt(text1);
-	it(`encript ${text1}`, function() {
-		if(a1!==result1){
-			throw new Error(`Expected ${result1}, but got ${a1}`);
-		}
+describe('CaesarCipher', () => {
+	[{
+		testCase: 'correct work with English text',
+		sourceText: 'meSSage',
+		shift: 5,
+		expectedResult: 'rjXXflj',
+	},
+	{
+		testCase: 'correct work with Russian text',
+		sourceText: 'сОобщЕние',
+		shift: 6,
+		expectedResult: 'чФфзяЛуол',
+	},
+	{
+		testCase: 'not set value shift',
+		sourceText: 'сОобщЕние',
+		shift: null,
+		expectedResult: 'чФфзяЛуол',
+	},
+	{
+		testCase: 'in the text a space',
+		sourceText: 'сОоб щЕние',
+		shift: 6,
+		expectedResult: 'чФфзяЛуол',
+	},
+	{
+		testCase: 'in the text a space',
+		sourceText: 'meS Sage',
+		shift: 5,
+		expectedResult: 'rjXXflj',
+	}].forEach(({ testCase, sourceText, shift, expectedResult }) => {
+		it(testCase, () => {
+			const cipher = new CaesarCipher(shift);
+			assert.notStrictEqual(cipher.shift, null, 'test: invalid shift');
+			const resultEncrypt = cipher.encrypt(sourceText);
+			assert.strictEqual(resultEncrypt, expectedResult, 'test: invalid encryption result');
+			const resultDecrypt = cipher.encrypt(resultEncrypt);
+			assert.strictEqual(resultDecrypt, sourceText, 'test: invalid decryption result');
+		});
 	});
-	const r1 = cipher.decrypt(a1);
-	it(`decript ${a1}`, function() {
-		if(r1!==text1){
-			throw new Error(`Expected ${r1}, but got ${text1}`);
-		}
-	});
-	cipher.shift = 6;
-	const a2 = cipher.encrypt(text2);
-	it(`encript ${text2}`, function() {
-		if(a2!==result2){
-			throw new Error(`Expected ${result2}, but got ${a2}`);
-		}
-	});
-	const r2 = cipher.decrypt(a2);
-	it(`decript ${r2}`, function() {
-		if(r2!==text2){
-			throw new Error(`Expected ${r2}, but got ${text2}`);
-		}
-	});
-});
+})
