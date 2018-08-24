d7926cfc9dd664e4205fb525edfa103e060bfe64
Lena <lenaLatkovich@gmail.com>
2018-08-24 15:53:51 +0300

    fix exceptions and tests

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import CaesarCipher from './lib/caesar-cipher';
 
-const cipher = new CaesarCipher(5);
-const a1 = cipher.encrypt('m1eSSa ge');
-console.log(a1);
-console.log(cipher.decrypt(a1));
+const cipher = new CaesarCipher(6);
+const a1 = cipher.encrypt('сОобщЕние');
+if (a1 !== undefined) {
+	console.log(a1);
+	const d1 = cipher.decrypt(a1);
+	if (d1 !== undefined) console.log(d1);
+}
 cipher.shift = null;
-const a2 = cipher.encrypt('сОобщЕние');
-console.log(a2);
-console.log(cipher.decrypt(a2));
+const a2 = cipher.encrypt('сОоб щЕние');
+if (a2 !== undefined) {
+	console.log(a2);
+	const d2 = cipher.decrypt(a2);
+	if (d2 !== undefined) console.log(d2);
+} 
--- test/test.js
+++ test/test.js
@@ -3,42 +3,59 @@ import assert from 'assert';
 
 describe('CaesarCipher', () => {
 	[{
-		testCase: 'correct work with English text',
+		testCase: 'Correct work with English text',
 		sourceText: 'meSSage',
 		shift: 5,
 		expectedResult: 'rjXXflj',
 	},
 	{
-		testCase: 'correct work with Russian text',
+		testCase: 'Correct work with Russian text',
 		sourceText: 'сОобщЕние',
 		shift: 6,
 		expectedResult: 'чФфзяЛуол',
 	},
 	{
-		testCase: 'not set value shift',
+		testCase: 'Not set value shift',
 		sourceText: 'сОобщЕние',
 		shift: null,
-		expectedResult: 'чФфзяЛуол',
+		expectedResult: undefined,
 	},
 	{
-		testCase: 'in the text a space',
+		testCase: 'In the text a space',
 		sourceText: 'сОоб щЕние',
 		shift: 6,
-		expectedResult: 'чФфзяЛуол',
+		expectedResult: undefined,
 	},
 	{
-		testCase: 'in the text a space',
+		testCase: 'In the text a space',
 		sourceText: 'meS Sage',
 		shift: 5,
-		expectedResult: 'rjXXflj',
+		expectedResult: undefined,
+	},
+	{
+		testCase: 'An invalid character was entered',
+		sourceText: 'сОоб2щЕние',
+		shift: 6,
+		expectedResult: undefined,
+	},
+	{
+		testCase: 'An invalid character was entered',
+		sourceText: 'сОоб_щЕние',
+		shift: 6,
+		expectedResult: undefined,
 	}].forEach(({ testCase, sourceText, shift, expectedResult }) => {
 		it(testCase, () => {
 			const cipher = new CaesarCipher(shift);
-			assert.notStrictEqual(cipher.shift, null, 'test: invalid shift');
-			const resultEncrypt = cipher.encrypt(sourceText);
-			assert.strictEqual(resultEncrypt, expectedResult, 'test: invalid encryption result');
-			const resultDecrypt = cipher.encrypt(resultEncrypt);
-			assert.strictEqual(resultDecrypt, sourceText, 'test: invalid decryption result');
+			if (cipher.shift === null) {
+				assert.strictEqual(cipher.shift, null, 'test: invalid shift');
+			} else {
+				const resultEncrypt = cipher.encrypt(sourceText);
+				assert.strictEqual(resultEncrypt, expectedResult, 'test: invalid encryption result');
+				if (resultEncrypt !== undefined) {
+					const resultDecrypt = cipher.decrypt(resultEncrypt);
+				assert.strictEqual(resultDecrypt, sourceText, 'test: invalid decryption result');
+				}
+			}
 		});
 	});
 })
